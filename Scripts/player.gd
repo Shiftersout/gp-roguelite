ba6be3f19c631ec81bdffc712e0b1e8b880d355c
Filipe Gonçalves <lipegoncalveso@gmail.com>
2023-07-13 19:28:47 -0300

    Added animation
    -basic player animations
    -test tilemap
    -improved movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,30 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 100
+@export var move_speed : float = 128
+@onready var sprite = $AnimatedSprite2D
+var h_direction = 1
 
 func _physics_process(_delta):
+	move()
+	flipSprite()
+	move_and_slide()
+
+func move():
 	var input_direction = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
 
-	velocity = input_direction * move_speed
+	velocity = input_direction.normalized() * move_speed
 
-	move_and_slide()
+func flipSprite():
+	if velocity == Vector2.ZERO:
+		sprite.play("idle")
+	else:
+		sprite.play("walking")
+		
+	if velocity.x > 0:
+		h_direction = 1
+	elif velocity.x < 0:
+		h_direction = -1
+		
+	sprite.scale.x = h_direction
