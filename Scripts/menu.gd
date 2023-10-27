7494d2bbf836675579b5bf55915c402d7188aa27
GustavoFBaratela <139513478+GustavoFBaratela@users.noreply.github.com>
2023-10-26 23:50:16 -0300

    Criado o menu principal
    Criado menu principal com os seus botões de redirecionamento

---
+++ Scripts/menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+
+#Descomentar quanto tiver a tela de créditosd
+#func _on_credits_pressed():
+	#get_tree().change_scene_to_file("res://Scenes/credits.tscn")
+
+#Descomentar quando tiver a tela de opções
+#func _on_options_pressed():
+	#get_tree().change_scene_to_file("Res://Scenes/options.tscn")
+	
+
+func _on_quit_pressed():
+	get_tree().quit()
