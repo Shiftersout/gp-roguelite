b23b443f95b82d50258a19ae73bc9024f26775dc
GustavoFBaratela <139513478+GustavoFBaratela@users.noreply.github.com>
2023-11-04 16:29:24 -0300

    Troca de fonte e Submenu de Opções
    Criado o submenu de opções e coloquei uma fonte 8 bit temporária:
    https://www.dafont.com/pt/minecraftia.font

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _on_play_pressed():
+
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 
 #Descomentar quanto tiver a tela de créditosd
@@ -8,9 +9,10 @@ func _on_play_pressed():
 	#get_tree().change_scene_to_file("res://Scenes/credits.tscn")
 
 #Descomentar quando tiver a tela de opções
-#func _on_options_pressed():
-	#get_tree().change_scene_to_file("Res://Scenes/options.tscn")
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
 	
 
 func _on_quit_pressed():
 	get_tree().quit()
+
