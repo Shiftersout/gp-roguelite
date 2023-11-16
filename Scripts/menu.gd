3e255f89bc6f9c615b1fb79aa2df0da13656d7cc
Filipe Gonçalves <lipegoncalveso@gmail.com>
2023-11-15 21:31:58 -0300

    Added tutorial

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -12,7 +12,8 @@ func _on_credits_pressed():
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Menus/options.tscn")
 	
-
 func _on_quit_pressed():
 	get_tree().quit()
 
+func _on_tutorial_pressed():
+	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
